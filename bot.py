5d824143f98bbf216cae33ad6cbf71b8568417de
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-02-10 19:04:46 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -90,7 +90,7 @@ async def start(event):
   if event.is_private:
     async for usr in client.iter_participants(event.chat_id):
      ad = f"[{usr.first_name}](tg://user?id={usr.id}) "
-     await client.send_message(-1001742841165, f"ℹ️ **Yeni Kullanıcı -** {ad}")
+     await client.send_message(f"ℹ️ **Yeni Kullanıcı -** {ad}")
      return await event.reply(f"💖😋 **Merhaba ben Fearless Tagger Bot** \n🏷 **Gruplarda kullanıcıları etikelemek için tasarlandım**. \n**Butonları kullanarak yardım alabilirsin.**", buttons=(
                       [
                        Button.inline("📚 Commands", data="komutlar")
