58743e9f6e78fa63d6af8582439a6bf89af1ff33
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-02-14 01:47:21 +0400

    Update random.py

--- mesaj/random.py
+++ mesaj/random.py
@@ -1,1 +1,65 @@
-
+taım = (
+"https://t.me/addtheme/sf158WSw7LWOtpvV",
+"https://t.me/addtheme/bpcrFtP4qYu0DdnJ",
+"https://t.me/addtheme/aUFKCX7AQ3aQpDjp",
+"https://t.me/addtheme/L7HVQjC4UUyOfL9y",
+"https://t.me/addtheme/Qd4eBWTIOH4Ai3Zv",
+"https://t.me/addtheme/NightWolf",
+"https://t.me/addtheme/GreenBlack",
+"https://t.me/addtheme/TvldPzYmpG8LqkY3",
+"https://t.me/addtheme/Q4GuvNPpMvG59G6V",
+"https://t.me/addtheme/kGQaW0HHsjc7oFOv",
+"https://t.me/addtheme/z3E6vkceX0pfmo5P",
+"https://t.me/addtheme/poMW3amfnwUwOefI",
+"https://t.me/addtheme/l1felAbEVNQCN3NW",
+"https://t.me/addtheme/y6xMaSuBOmuGekHj",
+"https://t.me/addtheme/Fp6h6JpzXrWnjF9y",
+"https://t.me/addtheme/Purple_Grapes",
+"https://t.me/addtheme/xQNP1Jp2aklmldNx",
+"https://t.me/addtheme/ry0AgHsISs439fxi",
+"https://t.me/addtheme/ZHl93FYO9ja7hN81",
+"https://t.me/addtheme/gc2MlPyKHMBjw5WS",
+"https://t.me/addtheme/ciNZt5N6QCFjsrQI",
+"https://t.me/addtheme/bEKOF0v8XuLAFZ6P",
+"https://t.me/addtheme/IOSTelegramThemes2020_11july",
+"https://t.me/addtheme/DarkPink_1",
+"https://t.me/addtheme/Halloween_04",
+"https://t.me/addtheme/BlackBlue_ByYamila",
+"https://t.me/addtheme/NewYorkNyVK",
+"https://t.me/addtheme/blackcircles_ByYamila",
+"https://t.me/addtheme/KINGByVK",
+"https://t.me/addtheme/MRPERFECTBYVK",
+"https://t.me/addtheme/ChanchiNeonByVK",
+"https://t.me/addtheme/SamurayByVK",
+"https://t.me/addtheme/NeonRocks_ByYamila",
+"https://t.me/addtheme/StichOhanaByVK",
+"https://t.me/addtheme/SkullDarkByVK",
+"https://t.me/addtheme/RedGirlByVK",
+"https://t.me/addtheme/SpidermanByVK",
+"https://t.me/addtheme/CuteMelodyByVK",
+"https://t.me/addtheme/YouAreBeautifulStichByVK",
+"https://t.me/addtheme/ManchesterUnitedByVK",
+)  
+  
+  
+azz = (
+"https://t.me/setlanguage/az-beta",
+)
+ 
+trrr = (
+"https://t.me/setlanguage/tr-beta",
+)
+ 
+russ = (
+"https://t.me/setlanguage/ru-beta",
+)
+ 
+ 
+enn = (
+"https://t.me/setlanguage/en-beta",
+)
+ 
+ 
+fra = (
+"https://t.me/setlanguage/fr-beta",
+)
