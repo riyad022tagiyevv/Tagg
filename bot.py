1e1459a72450ff0ec4df3662788196263993b850
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-02-16 12:40:39 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -228,11 +228,25 @@ async def meslehet(_, message):
     await message.reply_text((await random_line('kolge/txt/meslehet.txt')))
  
  
-@app.on_message(filters.command("sehid"))
-async def meslehet(_, message):
-    await message.reply_text((await random_line('kolge/txt/sehid.txt')))
+
  
- 
+button = InlineKeyboardMarkup([
+    [InlineKeyboardButton("🔄 Dəyiş", callback_data="deyis")]
+])
+
+@app.on_message(filters.command("sehid") & ~filters.edited)
+async def commit(_, message):
+    await message.reply_text((await random_line('kolge/txt/sehid.txt')), reply_markup=button)
+
+
+
+@app.on_callback_query(filters.regex("deyis"))
+async def deyis(_, query: CallbackQuery):
+    await query.edit_message_text((await random_line('kolge/txt/sehid.txt')), reply_markup=button)
+
+
+
+
 
 
 @client.on(events.NewMessage())
