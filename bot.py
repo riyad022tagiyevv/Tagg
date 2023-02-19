fe84c539bac533eeebf838165ff6173a36ee4439
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-02-19 12:30:57 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -234,20 +234,7 @@ async def bio(_, message):
                 await message.reply_photo(
                 "https://te.legra.ph/file/7c24db2c84218935a8ac4.jpg",
                 caption=(f"""**Salam {message.from_user.mention} 🎵\nMən !\nSəsli söhbətlərdə musiqi oxuyan botam. Ban yetkisiz, Səs yetkisi verib, Asistanı qrupa əlavə edin.\n\nSahibim👉  [Ulvi](https://t.me/BrendUIvi)**"""),
-         reply_markup=InlineKeyboardMarkup(
-            [
-                
-                [
-                    InlineKeyboardButton(
-                        "🔐 BAĞLA" , callback_data= "close"
-                    )]
-                    
-                    
-                ]
-                
-           ]
-        ),
-    )
+         reply_markup=bio)
   
 
 
