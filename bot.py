1cb686dc03bcffbda2331f2a261611ae910bad2e
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-02-20 02:24:01 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1171,44 +1171,6 @@ async def cancel(event):
 
 
 
-@app.on_message(filters.command("ship"))
-async def my_handler(client, msj):
-    chat_id = msj.chat.id
- 
-    BU_QRUP_USERLERI = []
-    async for member in client.iter_chat_members(chat_id):
-        if member.user.is_bot == True:
-            pass
-        elif member.user.is_bot == False:
-            BU_QRUP_USERLERI.append((member.user.mention))
- 
-    rnduser = random.choice(BU_QRUP_USERLERI)
-    sevgi2 = random.choice(BU_QRUP_USERLERI)
- 
-    if rnduser == sevgi2:
-        rnduser = random.choice(BU_QRUP_USERLERI)
-        # sevgi2 = random.choice(BU_QRUP_USERLERI)
-        if rnduser == sevgi2:
-            rnduser = random.choice(BU_QRUP_USERLERI)
-            # sevgi2 = random.choice(BU_QRUP_USERLERI)
-            if rnduser == sevgi2:
-                rnduser = random.choice(BU_QRUP_USERLERI)
-                if rnduser == sevgi2:
-                    await client.send_message(chat_id, f"{msj.from_user.mention} yeniden cehd edin")
-                elif rnduser != sevgi2:
-                    await client.send_message(chat_id,
-                                              f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
-            elif rnduser != sevgi2:
-                await client.send_message(chat_id,
-                                          f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
-        elif rnduser != sevgi2:
-            await client.send_message(chat_id, f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
-    elif rnduser != sevgi2:
-        await client.send_message(chat_id, f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
-
-
-
-
 
 @client.on(events.NewMessage(pattern="^.admin ?(.*)"))
 async def tag_admin(event):
